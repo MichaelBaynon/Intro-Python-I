0ba271038217aeaa4cc0e7d07c6f7ad4f814f9c7
MichaelBaynon <baynonmichael@gmail.com>
2020-01-15 18:41:39 -0500

    complete

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -1,14 +1,19 @@
-"""
-Python makes performing file I/O simple. Take a look
-at how to read and write to files here:
-
-https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
-"""
+#  """
+# Python makes performing file I/O simple. Take a look
+# at how to read and write to files here:
+#
+# https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+# """
 
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+
+f = open("foo.txt")
+for line in f:
+    print(line)
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +21,12 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+g = open("bar.txt", "w")
+g.write(' jqwdoijqwdoijqwiodj. ')
+g.write(' jqwdoijqwdoijqwioawdawdawdawddj. ')
+g.write(' jqwdoijqwdoawdawdawdawdijqwiodj. ')
+g.close()
+
+g = open("bar.txt", "r")
+print(g.read())

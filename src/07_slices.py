63449f038d3df3a2ea7e46dc47c9e9b32f7ef304
MichaelBaynon <baynonmichael@gmail.com>
2020-01-14 19:13:22 -0500

    dictionaries done

--- src/07_slices.py
+++ src/07_slices.py
@@ -12,26 +12,34 @@ Use Python's slice syntax to achieve the following:
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print()
+# x = slice(1,2,2)
+# print(a[x])
+print(a[1])
 
 # Output the second-to-last element: 9
-print()
+y = slice(-2,-1,)
+print(a[y])
 
 # Output the last three elements in the array: [7, 9, 6]
-print()
+z = slice(3, 7)
+print(a[z])
 
 # Output the two middle elements in the array: [1, 7]
-print()
+x = slice(2, 4)
+print(a[x])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+r = slice(1, 7)
+print(a[r])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+x = slice(0, 5)
+print(a[x])
 
 # For string s...
 
 s = "Hello, world!"
 
 # Output just the 8th-12th characters: "world"
-print()
+x = slice(7, 12)
+print(s[x])
